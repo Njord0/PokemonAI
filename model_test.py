01e0e61d480c0e0b6979d3533628d9122a9243fc
Njörd <52111877+Njord0@users.noreply.github.com>
2024-05-26 22:51:13 +0200

    Update test

--- model_test.py
+++ model_test.py
@@ -13,7 +13,7 @@ def do_test():
 
         img_array = tf.expand_dims(img, 0)
 
-        predictions = model.predict(img_array)
+        predictions = model.predict(img_array, verbose=0)
         probabilities = tf.nn.softmax(predictions[0])
 
         predicted_class_index = np.argmax(probabilities)
@@ -21,7 +21,8 @@ def do_test():
 
         confidence_score = 100 * np.max(probabilities)
 
-        print(f"Image: {image_path}, Predicted class: {predicted_class_name}, Confidence score: {confidence_score} ")
+        print(f'Image: {image_path.parts[-1]}')
+        print(f"Pokémon: {predicted_class_name}, Probabilité: {confidence_score}\n")
 
 if __name__ == '__main__':
     do_test()
